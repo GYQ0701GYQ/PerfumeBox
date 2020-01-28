321a889085da4656db8b7799bcb11032156efddc
GYQ0701GYQ <GYQ0701GYQ@163.com>
2020-01-28 10:33:04 +0800

    初始化的vue工程，运行效果是图片以及下方demo语句，主程序是app.vue，引用的component是hello.vue，刚安装vue是引用的是helloworld.vue,这是修改后的版本

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<!--展示模板-->
+<template>
+  <div id="app">
+    <img src="./assets/demo1.jpg" width="1530px">
+<!--    <router-view/>-->
+    <Hello></Hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+export default {
+  name: 'App',
+  components: {
+    Hello
+  }
+}
+</script>
+<!--样式代码-->
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+</style>
