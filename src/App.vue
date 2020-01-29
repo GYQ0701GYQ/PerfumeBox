54df3f37b7d1fcb605c54902ce60c8325707bd59
GYQ0701GYQ <GYQ0701GYQ@163.com>
2020-01-29 23:34:27 +0800

    assets中增加了banner区域的logo和banner背景图，App.vue当前显示状态是搜索栏在banner中央并有logo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <!--展示模板-->
 <template>
-  <div id="app">
-    <img src="./assets/demo1.jpg" width="530px">
-    <b>TESSSSST</b>
+  <div id="app" >
+    <div id="top" :style="backgroundDiv">
+    <img id="xiangshui_logo" src="./assets/xiangshui_icon.png" alt="logo图片" width="80px" >
+    <div id="searchbox" >
+      <input type="text" name="search" placeholder="请输入关键字">
+      <div id="search">搜索</div>
+    </div>
+    </div>
 <!--    <router-view/>-->
     <Hello>
-      <b>TEEEEST</b>
     </Hello>
   </div>
 </template>
@@ -16,6 +20,13 @@ export default {
   name: 'App',
   components: {
     Hello
+  },
+  data () {
+    return {
+      backgroundDiv: {
+        backgroundImage: 'url(' + require('./assets/banner2.jpg') + ')'
+      }
+    }
   }
 }
 </script>
@@ -27,6 +38,44 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 20px;
+  margin-top: 0px;
+}
+#top{
+  height: 300px;
+  /*width: 1920px;*/
+  background: center;
+  background-size: 100% 100%;
+}
+#xiangshui_logo{
+  padding:15px ;
+}
+#searchbox{
+  width: 380px;
+  margin:auto;
+  font-family: 'Microsoft YaHei';
+  font-size: 14px;
+  padding:40px ;
+}
+input{
+  width: 260px;
+  border: 1px solid #e2e2e2;
+  height: 30px;
+  float: left;
+  background-image: url(./assets/search.png);
+  background-repeat: no-repeat;
+  background-size: 25px;
+  background-position:5px center;
+  padding:0 0 0 40px;
+}
+#search{
+  width: 78px;
+  height: 32px;
+  float: right;
+  background: darkgray;
+  color: white;
+  text-align: center;
+  line-height: 32px;
+  cursor: pointer;
+  font-family: 'Microsoft YaHei';
 }
 </style>
