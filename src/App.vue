1a189d064729dbf72b8b39a0b2ad9ecf30dddb52
GYQ0701GYQ <GYQ0701GYQ@163.com>
2020-01-31 23:11:34 +0800

    增加了主页的css文件，在App.vue中外部引入该样式表。目前运行效果是有banner、搜索框、网站logo、导航栏(可悬停在顶部)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,34 @@
-<!--展示模板-->
 <template>
   <div id="app" >
-    <div id="top" :style="backgroundDiv">
-    <img id="xiangshui_logo" src="./assets/xiangshui_icon.png" alt="logo图片" width="80px" >
-    <div id="searchbox" >
-      <input type="text" name="search" placeholder="请输入关键字">
-      <div id="search">搜索</div>
+    <div id="banner" :style="backgroundDiv">
+      <div id="banner_line1">
+        <div id="xinshougroup">
+         <img id="xinshou_logo" class="sameline" src="./assets/xinshou.png" alt="新手提示" height="30px">
+         <table class="sameline">新手入门</table>
+        </div>
+        <img id="perfumebox_logo" src="./assets/perfumebox-logo-18.png" alt="logo图片" width="160px" >
+        <div id="logingroup">
+          <img id="yonghu_logo" class="sameline" src="./assets/yonghu.png" alt="个人中心" height="30px">
+          <table class="sameline">用户中心</table>
+        </div>
+      </div>
+      <div id="searchgroup" >
+        <input type="text" id="search" name="search" placeholder="请输入关键字">
+        <div id="searchbtn">搜索</div>
+      </div>
     </div>
-    </div>
-<!--    <router-view/>-->
+      <div id="navigation">
+        <ul :class="isFixed?'navigationFixed':'navigationUnFixed'">
+          <li>首 页</li>
+          <li>排 行</li>
+          <li>香 调</li>
+          <li>气 味</li>
+          <li>调 香 师</li>
+        </ul>
+      </div>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <!--    <router-view/>-->
     <Hello>
     </Hello>
   </div>
@@ -24,58 +44,28 @@ export default {
   data () {
     return {
       backgroundDiv: {
-        backgroundImage: 'url(' + require('./assets/banner2.jpg') + ')'
+        backgroundImage: 'url(' + require('./assets/banner3.png') + ')'
+      },
+      isFixed: false
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll () {
+      var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+      var offsetTop = document.querySelector('#navigation').offsetTop
+      if (scrollTop >= offsetTop) {
+        this.isFixed = true
+      } else {
+        this.isFixed = false
       }
     }
   }
 }
 </script>
-<!--样式代码-->
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 0px;
-}
-#top{
-  height: 300px;
-  /*width: 1920px;*/
-  background: center;
-  background-size: 100% 100%;
-}
-#xiangshui_logo{
-  padding:15px ;
-}
-#searchbox{
-  width: 380px;
-  margin:auto;
-  font-family: 'Microsoft YaHei';
-  font-size: 14px;
-  padding:40px ;
-}
-input{
-  width: 260px;
-  border: 1px solid #e2e2e2;
-  height: 30px;
-  float: left;
-  background-image: url(./assets/search.png);
-  background-repeat: no-repeat;
-  background-size: 25px;
-  background-position:5px center;
-  padding:0 0 0 40px;
-}
-#search{
-  width: 78px;
-  height: 32px;
-  float: right;
-  background: darkgray;
-  color: white;
-  text-align: center;
-  line-height: 32px;
-  cursor: pointer;
-  font-family: 'Microsoft YaHei';
-}
+  @import "PerfumeBox-Home.css";
 </style>
