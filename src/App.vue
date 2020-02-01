a87497d6a5d5a7b052d14be5465a25bd02d9b533
GYQ0701GYQ <GYQ0701GYQ@163.com>
2020-02-01 20:59:22 +0800

    assets中删除了未用到的图片，App.vue当前和上一版本相比：主页有轮播图，可以自动播放，但没有左右控制按钮

--- src/App.vue
+++ src/App.vue
@@ -17,15 +17,24 @@
         <div id="searchbtn">搜索</div>
       </div>
     </div>
-      <div id="navigation">
-        <ul :class="isFixed?'navigationFixed':'navigationUnFixed'">
-          <li>首 页</li>
-          <li>排 行</li>
-          <li>香 调</li>
-          <li>气 味</li>
-          <li>调 香 师</li>
-        </ul>
-      </div>
+    <div id="navigation">
+      <ul :class="isFixed?'navigationFixed':'navigationUnFixed'">
+        <li class="navigation_li">首 页</li>
+        <li class="navigation_li">排 行</li>
+        <li class="navigation_li">香 调</li>
+        <li class="navigation_li">气 味</li>
+        <li class="navigation_li">调 香 师</li>
+      </ul>
+    </div>
+    <div class="container">
+<!--      <img src="https://img.xssdcdn.com/static/index/2.jpg" alt="">-->
+      <a v-show="i===index" v-for="(item,i) in arr" :key="item.id" :href="item.url">
+        <img :src="item.img" alt="">
+      </a>
+      <ul class="indexes">
+        <li v-for="(item,i) in arr" :key="item.id" :class="{active:i===index}"></li>
+      </ul>
+    </div>
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
     <!--    <router-view/>-->
@@ -46,11 +55,38 @@ export default {
       backgroundDiv: {
         backgroundImage: 'url(' + require('./assets/banner3.png') + ')'
       },
-      isFixed: false
+      isFixed: false,
+      index: 0,
+      arr: [
+        {
+          url: 'https://www.nosetime.com/wenzhang/230.html',
+          img: 'https://img.xssdcdn.com/static/index/2.jpg'
+        },
+        {
+          url: 'https://www.nosetime.com/wenzhang/355.html',
+          img: 'https://img.xssdcdn.com/static/index/3.jpg'
+        },
+        {
+          url: 'https://www.nosetime.com/wenzhang/240.html',
+          img: 'https://img.xssdcdn.com/static/index/4.jpg'
+        },
+        {
+          url: 'https://www.nosetime.com/wenzhang/283.html',
+          img: 'https://img.xssdcdn.com/static/index/5.jpg'
+        },
+        {
+          url: 'https://www.nosetime.com/wenzhang/273.html',
+          img: 'https://img.xssdcdn.com/static/index/6.jpg'
+        }
+      ]
     }
   },
   mounted () {
     window.addEventListener('scroll', this.handleScroll)
+    setInterval(() => {
+      ++this.index
+      if (this.index > this.arr.length - 1) this.index = 0
+    }, 4000)
   },
   methods: {
     handleScroll () {
