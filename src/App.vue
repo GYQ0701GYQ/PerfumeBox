8c64301fdf028df47126ba901ed7f2590e327c2f
GYQ0701GYQ <GYQ0701GYQ@163.com>
2020-02-03 22:13:10 +0800

    App.vue当前和上一版本相比：在template中编写了主页精选文章的展示，但想尝试使用二维数组和v-for指令实现，App.vue代码中后半部分的注释是图片3D动态的代码，还没有插入到现有代码中，scratch.vue代码是3D代码修改前的版本

--- src/App.vue
+++ src/App.vue
@@ -34,12 +34,55 @@
         <li v-for="(item,i) in arr" :key="item.id" :class="{active:i===index}" @mouseover="mouseover_li(i)"></li>
       </ul>
     </div>
+    <div class="article_index">
+      <ul class="article_ul">
+        <li >
+          <a href='/wenzhang/364.html' class='article_img'>
+            <img src="https://img.xssdcdn.com/article/346/0.jpg!l" alt="" width="400px" >
+          </a>
+          <div class="article_title"><a href='/wenzhang/364.html' class="article_title">12支香水环游地球: 雪山、沙漠、海洋...</a></div>
+        </li>
+        <li >
+          <a href='/wenzhang/364.html' class='article_img'>
+            <img src="https://img.xssdcdn.com/article/363/0.jpg!l" alt="" width="400px" >
+          </a>
+          <div class="article_title"><a href='/wenzhang/364.html' class="article_title">穿越时间的爱与香气: 遇见世界上另一个馥郁的自己</a></div>
+        </li>
+        <li >
+          <a href='/wenzhang/364.html' class='article_img'>
+            <img src="https://img.xssdcdn.com/article/335/0.jpg!l" alt="" width="400px" >
+          </a>
+          <div class="article_title"><a href='/wenzhang/364.html' class="article_title">2019年度香水盘点：今年哪些香水能够杀出重围？</a></div>
+        </li>
+      </ul>
+      <ul class="article_ul">
+        <li >
+          <a href='/wenzhang/364.html' class='article_img'>
+            <img src="https://img.xssdcdn.com/article/353/2.jpg!l" alt="" width="400px" >
+          </a>
+          <div class="article_title"><a href='/wenzhang/364.html' class="article_title">芦丹氏2019全系列点评</a></div>
+        </li>
+        <li >
+          <a href='/wenzhang/364.html' class='article_img'>
+            <img src="https://img.xssdcdn.com/article/359/0.jpg!l" alt="" width="400px" >
+          </a>
+          <div class="article_title"><a href='/wenzhang/364.html' class="article_title">治愈系香水TOP 10: 寻找嗅觉的“深夜食堂”</a></div>
+        </li>
+        <li >
+          <a href='/wenzhang/364.html' class='article_img'>
+            <img src="https://img.xssdcdn.com/article/358/0.jpg!l" alt="" width="400px" >
+          </a>
+          <div class="article_title"><a href='/wenzhang/364.html' class="article_title">一杯凉白开，给你温暖的家</a></div>
+        </li>
+      </ul>
+    </div>
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
     <!--    <router-view/>-->
     <Hello>
     </Hello>
   </div>
+
 </template>
 
 <script>
@@ -77,6 +120,30 @@ export default {
           url: 'https://www.nosetime.com/wenzhang/273.html',
           img: 'https://img.xssdcdn.com/static/index/6.jpg'
         }
+      ],
+      article_imgs: [
+        [
+          {
+            img: 'https://img.xssdcdn.com/article/334/0.jpg!l'
+          },
+          {
+            img: 'https://img.xssdcdn.com/article/333/0.jpg!l'
+          },
+          {
+            img: 'https://img.xssdcdn.com/article/322/0.jpg!l'
+          }
+        ],
+        [
+          {
+            img: 'https://img.xssdcdn.com/article/321/0.jpg!l'
+          },
+          {
+            img: 'https://img.xssdcdn.com/article/319/0.jpg!l'
+          },
+          {
+            img: 'https://img.xssdcdn.com/article/330/0.jpg!l'
+          }
+        ]
       ]
     }
   },
@@ -109,3 +176,127 @@ export default {
 <style>
   @import "PerfumeBox-Home.css";
 </style>
+
+<!--<template>-->
+<!--  <div class="overall">-->
+<!--    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>-->
+<!--    <div-->
+<!--      class="card-wrap"-->
+<!--      @mousemove="handleMouseMove"-->
+<!--      @mouseenter="handleMouseEnter"-->
+<!--      @mouseleave="handleMouseLeave"-->
+<!--      ref="card"-->
+<!--    >-->
+<!--      <div class="card" :style="cardStyle">-->
+<!--        <img class="card-bg" src="@/assets/demo1.jpg" :style="cardBgTransform" alt />-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--export default {-->
+<!--  computed: {-->
+<!--    mousePX () {-->
+<!--      return this.mouseX / this.width-->
+<!--    },-->
+<!--    mousePY () {-->
+<!--      return this.mouseY / this.height-->
+<!--    },-->
+<!--    cardStyle () {-->
+<!--      // 外层卡片偏移量-->
+<!--      let rX = this.mousePX * 30-->
+<!--      let rY = this.mousePY * -30-->
+<!--      return {-->
+<!--        transform: `rotateY(${rX}deg) rotateX(${rY}deg)`-->
+<!--      }-->
+<!--    },-->
+<!--    cardBgTransform () {-->
+<!--      // 内层图片偏移量-->
+<!--      let tX = this.mousePX * -40-->
+<!--      let tY = this.mousePY * -40-->
+<!--      return {-->
+<!--        transform: `translateX(${tX}px) translateY(${tY}px)`-->
+<!--      }-->
+<!--    }-->
+<!--  },-->
+<!--  data () {-->
+<!--    return {-->
+<!--      width: 0,-->
+<!--      height: 0,-->
+<!--      mouseX: 0,-->
+<!--      mouseY: 0,-->
+<!--      mouseLeaveDelay: null // 添加一个默认还原的定时器-->
+<!--    }-->
+<!--  },-->
+<!--  mounted () {-->
+<!--    this.width = this.$refs.card.offsetWidth-->
+<!--    this.height = this.$refs.card.offsetHeight-->
+<!--  },-->
+<!--  methods: {-->
+<!--    handleMouseMove (e) {-->
+<!--      // e.pageX 鼠标在body的x轴坐标位置-->
+<!--      // this.$refs.card.offsetLeft 外层卡片容器与body的左侧距离-->
+<!--      // e.pageX - this.$refs.card.offsetLeft 鼠标所在容器内的x轴坐标位置-->
+<!--      // e.pageX - this.$refs.card.offsetLeft - this.width / 2 鼠标以容器中心点为原点的x轴位置-->
+<!--      this.mouseX = e.pageX - this.$refs.card.offsetLeft - this.width / 2-->
+<!--      this.mouseY = e.pageY - this.$refs.card.offsetTop - this.height / 2-->
+<!--    },-->
+<!--    // 当鼠标进入时清除计时器-->
+<!--    handleMouseEnter () {-->
+<!--      clearTimeout(this.mouseLeaveDelay)-->
+<!--    },-->
+<!--    // 当鼠标离开时添加定时器-->
+<!--    handleMouseLeave () {-->
+<!--      this.mouseLeaveDelay = setTimeout(() => {-->
+<!--        this.mouseX = 0-->
+<!--        this.mouseY = 0-->
+<!--      }, 1000)-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+
+<!--<style lang="scss" scoped>-->
+<!--  .overall {-->
+<!--    perspective: 2000px;-->
+<!--  }-->
+<!--  .card-wrap {-->
+<!--    perspective: 2000px;-->
+<!--    transform-style: preserve-3d;-->
+<!--    cursor: pointer;-->
+<!--  }-->
+<!--  .card {-->
+<!--    transform-style: preserve-3d;-->
+<!--    width: 300px;-->
+<!--    height: 200px;-->
+<!--    background: #ececec;-->
+<!--    overflow: hidden;-->
+<!--    perspective: 1000px;-->
+<!--    transition: 1s cubic-bezier(0.23, 1, 0.32, 1);-->
+<!--    opacity: 0.8;-->
+<!--    margin: auto auto;// 有修改-->
+<!--  }-->
+<!--  .card-bg {-->
+<!--    transform-style: preserve-3d;-->
+<!--    width: 150%;-->
+<!--    height: 135%;-->
+<!--    display: block;-->
+<!--    position: relative;-->
+<!--    left: -25%;-->
+<!--    top: -25%;-->
+<!--    transition: 1s cubic-bezier(0.23, 1, 0.32, 1);-->
+<!--    //   pointer-events: none; //pointer-events 更像是JavaScript。用于阻止鼠标事件-->
+<!--  }-->
+<!--  //样式尽量定义成3层（否则会出现卡顿现象），1.鼠标经过开始层，2.卡片旋转层，3.图片旋转层-->
+<!--  //鼠标移入到1层时，执行过度效果（会有一个缓冲），过1s后缓冲结束，此时旋转时不再会有以上定义的1s过渡效果（不然会出现一卡一卡的情况），当鼠标移出时才会执行以上的过度效果，实现缓慢的归位状态（而不是一闪就归位了）-->
+<!--  //此处 hover选择器优先级最高-->
+<!--  .card-wrap:hover .card-bg {-->
+<!--    transition: 0.5s cubic-bezier(0.23, 1, 0.32, 1);-->
+<!--  }-->
+<!--  .card-wrap:hover .card {-->
+<!--    opacity: 1;-->
+<!--    transition: 0.5s cubic-bezier(0.23, 1, 0.32, 1);-->
+<!--    box-shadow: white 0 0 0 5px, rgba(0, 0, 0, 0.66) 0 30px 60px 0;-->
+<!--  }-->
+<!--</style>-->
