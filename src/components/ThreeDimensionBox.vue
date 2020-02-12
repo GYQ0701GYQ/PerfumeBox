9e9ed34d27a2b71da81900dfd3d840591cb9b43f
GYQ0701GYQ <GYQ0701GYQ@163.com>
2020-02-12 22:59:49 +0800

    vue组件和iframe页面传参：vue父页面可以读取子页面数据和传送数据到子页面，但子页面addListener监听并未修改实际参数（console有输出），父页面点击按钮触发函数时子页面有参数动态修改

--- src/components/ThreeDimensionBox.vue
+++ src/components/ThreeDimensionBox.vue
@@ -1,13 +1,58 @@
 <template>
   <div>
   <h1>这里是3D测试页面</h1>
-  <iframe src="../../static/ThreeDimensionBox/index.html" frameborder="0" style="width: 80%;height: 650px"></iframe>
+<!--  <iframe src="../../static/ThreeDimensionBox/index.html" frameborder="0" style="width: 80%;height: 650px"></iframe>-->
+  <el-button @click="getData()">点击获取iframe数据</el-button>
+  <el-button @click="setData('我是vue数据')">向iframe中发送数据</el-button>
+  <iframe id="mainIframe" ref="mainIframe" name="mainIframe" src="../../static/ThreeDimensionBox/index.html" frameborder="0" style="width: 80%;height: 650px" scrolling="auto" @load="loaded" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ThreeDimensionBox'
+  name: 'ThreeDimensionBox',
+  props: {
+  },
+  mounted() {
+    const mapFrame = this.$refs['mainIframe']
+    if (mapFrame.attachEvent) { // 兼容浏览器判断
+      mapFrame.attachEvent('onload', function() {
+        const iframeWin = mapFrame.contentWindow
+        iframeWin.postMessage({
+          method: 'getBaseInfo',
+          data:'初始化值1'
+        }, '*')
+        // data传递的参数   *写成子页面的域名或者是ip
+      })
+    } else {
+      mapFrame.onload = function() {
+        const iframeWin = mapFrame.contentWindow
+        iframeWin.postMessage({
+          method: 'getBaseInfo',
+          data:'初始化值2'
+        }, '*')
+      }
+    }
+  },
+  methods: {
+    loaded() {
+      const vm = this.$refs.mainIframe.contentWindow.vm
+      console.log('VUE页面加载',vm)
+      // console.log(this.$refs.mainIframe.contentWindow)
+      // vm.func1()
+    },
+    getData() {
+      const obj1 = window.frames['mainIframe']// 获得对应iframe的window对象
+      // alert(obj1.vm.aaaaa)
+      console.log('VUE获取iframe数据',obj1.vm.aaaaa)
+    },
+    setData(data) {
+      const obj1 = window.frames['mainIframe']// 获得对应iframe的window对象
+      // console.log(data)
+      obj1.vm.wpsData = data
+      console.log('VUE向iframe发送数据',obj1.vm.wpsData)
+    }
+  }
 }
 </script>
 
