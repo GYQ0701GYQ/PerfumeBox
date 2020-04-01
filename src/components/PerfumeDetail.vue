8d7108832f2f45c646aa479c82f685560db7ad9c
GYQ0701GYQ <GYQ0701GYQ@163.com>
2020-04-01 18:29:58 +0800

    【调整前端带中文参数调用后端接口】

--- src/components/PerfumeDetail.vue
+++ src/components/PerfumeDetail.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-      <h1>{{'搜索内容为'+this.$route.params.search_name}}</h1>
+      <h3>{{'搜索内容为：'+this.$route.params.search_name}}</h3>
+      <div>{{this.perfume_info}}</div>
     </div>
 </template>
 
@@ -13,13 +14,14 @@
       }
     },
     created () {
-      this.show_perfume(this.$route.params.search_name)
+      this.search_perfume(this.$route.params.search_name)  //向后端传递搜索框的字符串，按字符串检索
+      // this.test_show_perfume()  //不向后端传递参数的情况下直接get数据
     },
     methods:{
-      show_perfume(search_name){
+      search_perfume(search_name){
         this.$http({
-          url:'http://127.0.0.1:8000/api/add_book',
-          method:'POST',
+          url:'http://127.0.0.1:8000/api/search_perfume',
+          method:'GET',
           data:{
             search_info:String(search_name),
             search_info2 :333
@@ -35,6 +37,21 @@
               this.$message.success('调用成功')
             } else {
               this.$message.error('调用失败')
+            }
+          })
+      },
+      test_show_perfume(){
+        this.$http.get('http://127.0.0.1:8000/api/show_perfume')
+          .then((response) => {
+            console.log(response)
+            var res = JSON.parse(response.bodyText)
+            console.log(res)
+            if (res.error_num === 0) {
+              this.perfume_info = res['list']
+              console.log(this.perfume_info)
+            } else {
+              this.$message.error('查询香水失败')
+              console.log(res['msg'])
             }
           })
       }
